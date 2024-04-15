542ccfda9cc8498d55c6584f206c340de2d11aeb
Mohammed Faizan <mohdfaizan.zprb2@gmail.com>
2024-04-15 20:00:21 +0530

    Added logo to frontend landing page

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import React from "react";
+import paytmLogo from '../assets/paytm_icon-icons.com_62778.png'
 
 const LandingPage = () => {
   return (
@@ -7,12 +8,13 @@ const LandingPage = () => {
       <Header />
       <main className="flex items-center px-10 h-[60vh]">
         <section>
-          <h1 className="w-2/3 text-5xl font-bold">
+          <h1 className="w-2/3 text-6xl font-semibold">
             We basically made payments frictionless with paytm
           </h1>
         </section>
         <section>
-          <div className="text-white size-32 bg-slate-900 rounded-3xl ">P</div>
+          <img src={paytmLogo} className="w-8/12"/>
+          {/* <div className="text-white size-32 bg-slate-900 rounded-3xl ">P</div> */}
         </section>
       </main>
     </div>
